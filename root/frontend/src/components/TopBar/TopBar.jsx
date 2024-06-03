00a15be2c2621883e7bccbd0ce99caafbd11f2a0
Tex McGinley <79146117+TexMcGinley@users.noreply.github.com>
2024-06-03 16:26:41 +0200

    Styling changes TopBar

--- root/frontend/src/components/TopBar/TopBar.jsx
+++ root/frontend/src/components/TopBar/TopBar.jsx
@@ -5,14 +5,14 @@ const TopBar = ({ score, movesLeft, round, onSubmit }) => {
   return (
     <div className="top-bar">
       <div className="score-display">
-        SCORE: {score} | MOVES LEFT: {movesLeft}
+        SCORE: {score} | MOVES LEFT: {movesLeft}/20
       </div>
       <div className="top-bar-title">
         <h1>Movie Ranker</h1>
       </div>
       <div className="top-bar-buttons">
+        <div className="round-display">ROUND: {round}/5 </div>
         <button onClick={onSubmit}>SUBMIT</button>
-        <div className="round-display">ROUND: {round}</div>
       </div>
     </div>
   );
